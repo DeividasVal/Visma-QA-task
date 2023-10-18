152a485700155c1429492cdb5548da2bf9ce2997
Deividas Valacka <deividas.valacka@strongpoint.com>
2023-10-18 19:08:49 +0300

    Made the classes have their own .cs file

---
+++ Visma QA task/BandMember.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visma_QA_task
+{
+    internal class BandMember
+    {
+        public string Name { get; }
+        public string Role { get; }
+        public BandMember(string name, string role)
+        {
+            Name = name;
+            Role = role;
+        }
+    }
+}
